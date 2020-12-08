ae11ad53cbd6c9cbf4276e0ad0428ba8001b9486
Norman Fomferra <norman.fomferra@gmail.com>
2020-12-08 15:53:08 +0100

    added a little help

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
 # nc2zarr
 
-A Python tool that converts NetCDF files to Zarr format.
+A Python tool that converts multiple NetCDF files to single Zarr datasets.
+
+### Create Python environment
+
+    $ conda install -n base -c conda-forge mamba
+    $ cd nc2zarr
+    $ mamba env create
+    
+### Install nc2zarr from Sources 
+
+    $ cd nc2zarr
+    $ conda activate nc2zarr
+    $ python setup.py develop
+
+## Usage
+
+### Help
+
+    $ nc2zarr --help
+    Usage: nc2zarr [OPTIONS] [INPUT_FILE ...]
+    
+      Converts multiple NetCDF files to a single Zarr dataset.
+    
+      INPUT_FILE may refer to a NetCDF file or a glob that identifies multiple
+      them, e.g. "L3_SST/**/*.nc".
+    
+      If CONFIG_FILE is given, any given NetCDF file arguments overwrite setting
+      input/path in CONFIG_FILE . Accordingly the following option overwrite
+      settings in CONFIG_FILE:
+    
+      --mode overwrites mode
+      --batch overwrites input/batch_size (not implemented yet)
+      --decode-cf overwrites input/decode_cf
+      --output overwrites output/path
+    
+    Options:
+      -o, --output OUTPUT_FILE  Output name. Defaults to "out.zarr".
+      -c, --config CONFIG_FILE  Configuration file. Defaults to "nc2zarr-
+                                config.yml".
+    
+      -b, --batch BATCH_SIZE    Batch size. If greater zero, conversion will be
+                                performed in batches of the given size.
+    
+      -m, --mode MODE           Configuration file. Must be one of ('slices',
+                                'one_go'). Defaults to "slices".
+    
+      -d, --decode-cf           Decode variables according to CF conventions.
+      -v, --verbose             Print more output.
+      --version                 Show version number and exit.
+      --help                    Show this message and exit.
+
+### Config file format
+
+TODO
--- nc2zarr/cli.py
+++ nc2zarr/cli.py
@@ -30,12 +30,12 @@ from nc2zarr.constants import MODE_CHOICES
 
 
 @click.command(name='nc2zarr')
-@click.argument('input_files', nargs=-1, metavar='[INPUT_FILES ...]')
+@click.argument('input_files', nargs=-1, metavar='[INPUT_FILE ...]')
 @click.option('--output', '-o', 'output_file', metavar='OUTPUT_FILE',
               help=f'Output name. Defaults to "{DEFAULT_OUTPUT_FILE}".')
 @click.option('--config', '-c', 'config_file', metavar='CONFIG_FILE',
               help=f'Configuration file. Defaults to "{DEFAULT_CONFIG_FILE}".')
-@click.option('--batch', '-b', 'batch_size', type=int,
+@click.option('--batch', '-b', 'batch_size', metavar='BATCH_SIZE', type=int,
               help=f'Batch size. If greater zero, conversion will be performed in batches of the given size.')
 @click.option('--mode', '-m', 'mode', metavar='MODE', type=click.Choice(MODE_CHOICES),
               help=f'Configuration file. Must be one of {MODE_CHOICES}. Defaults to "{DEFAULT_MODE}".')
@@ -54,15 +54,27 @@ def main(input_files: List[str],
          verbose: bool,
          version: bool):
     """
-    Convert NetCDF files to Zarr format.
+    Converts multiple NetCDF files to a single Zarr dataset.
 
-    Optional INPUT_FILES may contain wildcards, e.g. "L3_SST/*.nc".
+    INPUT_FILE may refer to a NetCDF file or a glob that identifies multiple them, e.g. "L3_SST/**/*.nc".
+
+    If CONFIG_FILE is given, any given NetCDF file arguments overwrite setting input/path in CONFIG_FILE .
+    Accordingly the following option overwrite settings in CONFIG_FILE:
+
+    \b
+    --mode overwrites mode
+    --batch overwrites input/batch_size (not implemented yet)
+    --decode-cf overwrites input/decode_cf
+    --output overwrites output/path
     """
 
     if version:
         from nc2zarr.version import version
         print(version)
         return 0
+
+    if batch_size is not None:
+        raise click.ClickException('option --batch is not supported yet')
 
     from nc2zarr.convert import convert_netcdf_to_zarr
     from nc2zarr.perf import measure_time
