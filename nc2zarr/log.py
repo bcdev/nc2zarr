49ba930246901185e8c55438fb5c4a7aa0f316c7
Norman Fomferra <norman.fomferra@gmail.com>
2021-01-08 13:01:37 +0100

    Updated copyright

--- nc2zarr/log.py
+++ nc2zarr/log.py
@@ -1,5 +1,5 @@
 # The MIT License (MIT)
-# Copyright (c) 2020 by Brockmann Consult GmbH and contributors
+# Copyright (c) 2021 by Brockmann Consult GmbH and contributors
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
