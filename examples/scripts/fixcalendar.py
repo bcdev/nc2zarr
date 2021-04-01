51da3aaf9ee5b87c3096a0fb1d03749af759acaa
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2021-04-01 16:45:56 +0200

    fixcalendar script: add dry run option

--- examples/scripts/fixcalendar.py
+++ examples/scripts/fixcalendar.py
@@ -15,6 +15,9 @@ def main():
                         help="Read S3 configuration from this/these config(s)")
     parser.add_argument("--force", "-f", action="store_true",
                         help="Rename calendar even if it's already lower-case")
+    parser.add_argument("--dry-run", "-s", action="store_true",
+                        help="Don't rename, "
+                             "just show what would have been done")
     parser.add_argument("zarr_store", type=str,
                         help="Zarr store specifier (path or URL)")
     args = parser.parse_args()
@@ -27,7 +30,6 @@ def main():
             s3_config = config["output_s3"]
         # We have to create this store manually to set normalize_keys=False,
         # because normalize_keys=True can break consolidate_metadata.
-        print(s3_config)
         store = zarr.storage.FSStore(store_arg, mode="r+", **s3_config,
                                      normalize_keys=False)
     else:
@@ -41,11 +43,15 @@ def main():
         log("Already lower case; leaving unchanged.")
     else:
         new_calendar = calendar.lower()
-        log(f"Renaming to \"{new_calendar}\"...")
-        z.time.attrs["calendar"] = new_calendar
-        log("Consolidating...")
-        zarr.consolidate_metadata(store)
-        log("Done.")
+        if args.dry_run:
+            log(f"New name: \"{new_calendar}\"")
+            log("Dry run requested -- not actually renaming.")
+        else:
+            log(f"Renaming to \"{new_calendar}\"...")
+            z.time.attrs["calendar"] = new_calendar
+            log("Consolidating...")
+            zarr.consolidate_metadata(store)
+            log("Done.")
 
         
 def log(s):
