08b4f69cc869f610460ff4fcf2c50375bb1f79a4
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2021-06-03 13:49:23 +0200

    readzarrtest.py: add docstring and error count

--- examples/scripts/readzarrtest.py
+++ examples/scripts/readzarrtest.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+"""readzarrtest.py: test that a Zarr store is readable
+
+This script attempts to read one data value from each chunk in a specified
+Zarr store, and reports any errors encountered. It exits with status 0 if no
+read errors were encountered, 1 otherwise. Note that checking an entire store
+like this can be slow (especially for large or remote stores), and that many
+potential problems (e.g. missing chunks or incorrect data values) are not
+detected by this script.
+
+The Zarr path can be a local file. If the appropriate Python libraries
+and access credentials are present, http or S3 URLs may also be used.
+Examples:
+
+./readzarrtest.py /some/path/OCEANCOLOUR_ATL_CHL_L4_NRT_OBSERVATIONS_009_037.zarr
+
+./readzarrtest.py http://cop-services.s3.eu-central-1.amazonaws.com/OCEANCOLOUR_MED_OPTICS_L3_NRT_OBSERVATIONS_009_038.zarr/
+
+./readzarrtest.py s3://cop-services/LWQ-NRT-300m.zarr
+"""
+
 import sys
 import argparse
 import zarr
@@ -8,23 +28,23 @@ from typing import List
 
 def main():
     parser = argparse.ArgumentParser(
-        description="Test that each chunk of a zarr file is readable."
+        description="Test that each chunk of a zarr store is readable. "
+                    "See docstring in source code for more details."
     )
     parser.add_argument("zarr", type=str, metavar="PATH_OR_URL")
     args = parser.parse_args()
     zarr_file = args.zarr
     z = zarr.open(zarr_file, mode="r")
-    error = False
+    n_errors = 0
     for name, array in z.arrays():
-        error = error or read_data(name, array)
-    if not error:
-        print("No errors encountered.")
-    sys.exit(1 if error else 0)
+        n_errors += read_data(name, array)
+    print(f"{n_errors} error(s) encountered.")
+    sys.exit(1 if n_errors > 0 else 0)
 
 
-def read_data(array_name: str, array: zarr.Array) -> bool:
+def read_data(array_name: str, array: zarr.Array) -> int:
 
-    error = False
+    n_errors = 0
 
     def read_data_rec(coords: List[int], shape: List[int], chunks: List[int]):
         if len(shape) > 0:
@@ -42,11 +62,11 @@ def read_data(array_name: str, array: zarr.Array) -> bool:
                 _ = array[tuple(coords)]
             except Exception:
                 print(f"Error reading {array_name}{coords}")
-                nonlocal error
-                error = True
+                nonlocal n_errors
+                n_errors += 1
 
     read_data_rec([], list(array.shape), list(array.chunks))
-    return error
+    return n_errors
 
 
 if __name__ == "__main__":
