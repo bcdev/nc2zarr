1d45c592fb262bd85e9386b8e66f508f6062325e
Pontus Lurcock <pontus.lurcock@brockmann-consult.de>
2021-11-23 10:28:47 +0100

    Add flatten.py to example scripts
    This script rechunks a Zarr with chunks of size 1 in time, full size in
    latitude and longitude.

--- examples/scripts/flatten.py
+++ examples/scripts/flatten.py
@@ -7,28 +7,37 @@ will be read from standard environment variables or files (see s3fs docs).
 
 The output dataset will have the same data as the input dataset, rechunked
 so that the chunks are flat time slices. That is, the chunks will have
-size 1 in the time dimension and cover the full extent of the dataset in
-the lat and lon dimensions.
+size 1 in the time dimension.
+
+Spatially (i.e. in lat and lon dimensions), either the current chunk
+dimensions can be kept, or the chunk spatial dimensions can be set to cover
+the entire extent of the dataset.
 """
 
-import xarray as xr
 import argparse
+import xarray as xr
+import zarr
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--keep-spatial', '-k',
+                        action='store_true',
+                        help='Keep current spatial chunk size')
     parser.add_argument('input_zarr')
     parser.add_argument('output_zarr')
     args = parser.parse_args()
-    rechunk(args.input_zarr, args.output_zarr)
+    rechunk(args.input_zarr, args.output_zarr, args.keep_spatial)
 
 
-def rechunk(input_path, output_path):
+def rechunk(input_path: str, output_path: str, keep_spatial: bool):
     ds = xr.open_dataset(input_path, engine="zarr")
     for var in ds:
         del ds[var].encoding['chunks']
-    full_lat = len(ds.lat)
-    full_lon = len(ds.lon)
-    ds_rechunked = ds.chunk({'time': 1, 'lat': full_lat, 'lon': full_lon})
+    chunk_params = {'time': 1}
+    if not keep_spatial:
+        chunk_params['lat'] = len(ds.lat)
+        chunk_params['lon'] = len(ds.lon)
+    ds_rechunked = ds.chunk(chunks=chunk_params)
     print('Writing output Zarr...')
     ds_rechunked.to_zarr(output_path)
 
